8e61b6e4a616d3a3e20863460fb739ad029f641d
Isac Ekberg <isaek808@student.liu.se>
2016-03-23 16:40:57 +0100

    AWS settings

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ EB_BUCKET=crag-finder-dev
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp --region eu-west-1 $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name crag-finder-dev --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+aws elasticbeanstalk create-application-version --region eu-west-1 --application-name crag-finder --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name hello-env --version-label $SHA1
+aws elasticbeanstalk update-environment --region eu-west-1 --environment-name crag-finder-dev --version-label $SHA1
