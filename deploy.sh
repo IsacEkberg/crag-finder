baf03e90e82c80e65dcb5985aec2aac65405c52d
Isac Ekberg <isaek808@student.liu.se>
2016-03-23 14:17:39 +0100

    CircleCI and AWS deployment

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 SHA1=$1
 
 # Deploy image to Docker Hub
-docker push circleci/hello:$SHA1
+docker push cragfinder/development:$SHA1
 
 # Create new Elastic Beanstalk version
-EB_BUCKET=hello-bucket
+EB_BUCKET=crag-finder-dev
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
