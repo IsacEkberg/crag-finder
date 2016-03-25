6c9ce0785366ab9ffd8b629cedb77815ea2637bb
Isac Ekberg <isaek808@student.liu.se>
2016-03-25 13:21:35 +0100

    Added error page for local development

--- django_app/views.py
+++ django_app/views.py
@@ -1,9 +1,15 @@
-from django.http import JsonResponse
-from django.shortcuts import redirect
+from django.http import JsonResponse, HttpResponse
 
 
 def development_index(request):
-    return redirect('static/index.html')
+    return HttpResponse("<p><b>Error: Wrong port!</b> <br> In order to run this project locally:</p>"
+                        "<ol>"
+                        "<li>Run nginx</li>"
+                        "<li>Run 'ember server'</li>"
+                        "<li>Run djangos 'manage.py runserver'</li>"
+                        "</ol>"
+                        "<p>For more details, checout the nginx_local_dev folder.</p>"
+                        "<p>(If you have already done this, try 127.0.0.1:1337 instead)")
 
 
 def development_api_mock(request):
