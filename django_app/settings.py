fcee372eeb1b56c906fb3afa724dbf65ba44da5a
Isac Ekberg <isaek808@student.liu.se>
2016-03-27 15:24:28 +0200

    Change to nginx configuration
    Must allow django to serve static files in local development.

--- django_app/settings.py
+++ django_app/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/django_static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "ember_app/dist")]
 if LOCAL_DEV or LOCAL_DOCKER:
     STATIC_ROOT = os.path.join(BASE_DIR, "static_content")
