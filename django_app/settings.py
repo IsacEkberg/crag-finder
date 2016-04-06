9f38922178e359d35fc81e5ac83b52d861eff008
jonathan <jonathan@jonathananderson.se>
2016-04-07 00:08:48 +0200

    Translated all fields and models to swedish and changed Django language to swedish

--- django_app/settings.py
+++ django_app/settings.py
@@ -173,9 +173,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'sv-se'  # en-us
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Stockholm'
 
 USE_I18N = True
 
