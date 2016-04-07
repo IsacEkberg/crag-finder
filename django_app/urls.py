7da34d740d7bdf35577f00c61de90e1aa3e20504
jonathan <jonathan@jonathananderson.se>
2016-04-07 23:39:06 +0200

    Added reset of password

--- django_app/urls.py
+++ django_app/urls.py
@@ -18,9 +18,15 @@ from django.contrib.staticfiles import views
 from django.conf.urls import url, include
 from django.conf import settings
 
-from .views import development_index, development_api_mock
+from .views import development_index, development_api_mock, reset, reset_done, reset_confirm, reset_complete
 from django_api.admin import cragfinder_admin_site
+
 urlpatterns = [
+    url(r'^admin/password_reset_admin/$', view=reset, name='admin_password_reset'),
+    url(r'^admin/password_reset/$', view=reset, name='password_reset'),
+    url(r'^admin/password_reset/done/$', view=reset_done, name='password_reset_done'),
+    url(r'^admin/password_reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', view=reset_confirm, name='password_reset_confirm'),
+    url(r'^admin/password_reset/complete/$', view=reset_complete, name='password_reset_complete'),
     url(r'^admin/', cragfinder_admin_site.urls),
     url(r'^api/', include('django_api.urls'))
 ]
