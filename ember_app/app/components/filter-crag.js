d1387b4206605140dd7f0daa5387db9d07d625bc
IsacEkberg <isac.ekberg@gmail.com>
2016-04-09 22:56:57 +0200

    Redid front page. Autocomplete works better for mobile.

--- ember_app/app/components/filter-crag.js
+++ ember_app/app/components/filter-crag.js
@@ -1,11 +1,34 @@
 import Ember from 'ember';
+var $ = Ember.$;
 
 export default Ember.Component.extend({
   filter: null,
+  dropdownClass: Ember.computed('focused', function () {
+    if( this.get('focused') ) {
+      return 'open';
+    } else {
+      return '';
+    }
+  }),
   filteredList: null,
+  focused: false,
+
+  moveFocusDown: function () {
+    console.log("Moving on.");
+    $('.dropdown-menu').children().first().children().focus();
+  },
+
   actions: {
-    autoComplete() {
-      this.get('autoComplete')(this.get('filter'));
+    autoComplete(param, e) {
+      var self = this;
+      if(e.keyCode === 40){
+        console.log("Should move...");
+        self.get('moveFocusDown')();
+      }
+      self.get('autoComplete')(this.get('filter'));
+    },
+    focus() {
+      this.set('focused', true);
     }
   }
 });
--- todo.md
+++ todo.md
@@ -9,13 +9,18 @@ __MVP:__
 - Clean up django-admin. J
 - content authoring system in django-admin. (Area managers, groups, reversion, user admin group)
 - Secure POST to Django-REST
-- Create area view (mobile). I
-- Create directions view (mobile). I
 - Create routes view (mobile). I
 - add nr to routes (django field, should be user specified, and allow stuff like: '13a' & '13b')
 - Enable password reset. J
 - Show rockface pictures. (How to find url? Pass it in DRF?) I/J
 - Picture upload (Images of rockfaces). J
+
+
+__Frontend:__
+- Search box on front page. (Mobile friendly!)
+- Min-height on area page.
+- Load google maps properly
+
 
 
 __Later:__
